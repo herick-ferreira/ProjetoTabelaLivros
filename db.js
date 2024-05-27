381966c21d4028b7f7661e0cf7e3029c11ebc88b
Herick <77906684+herick-ferreira@users.noreply.github.com>
2024-05-27 16:10:38 -0300

    Update db.js

--- db.js
+++ db.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const dbURI = 'mongodb://localhost:27017/bookstore';
 
+// const dbURI = 'mongodb+srv://herickfr223:JLQqZ26xWysi0LH4@cluster0.1ip671l.mongodb.net/bookstore?retryWrites=true&w=majority&appName=Cluster0';
+
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected...'))
     .catch(err => console.log(err));
